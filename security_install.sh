a956206ebbf408f761327d052b55a001f1ce224e
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2023-04-25 11:21:24 -0500

    Update security_install.sh

--- security_install.sh
+++ security_install.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get upgrade -y
 
 # Set password requirements
 echo "Setting password requirements..."
-sed -i 's/password.*requisite.*pam_pwquality.so.*/password requisite pam_pwquality.so minlen=8 minclass=0 lcredit=0 ucredit=0 dcredit=0 ocredit=0/' /etc/pam.d/common-password
+sed -i 's/password.*requisite.*pam_pwquality.so.*/password requisite pam_pwquality.so minlen=8 minclass=0 lcredit=0 ucredit=0 dcredit=0 ocredit=0 dictpath=\/dev\/null/' /etc/pam.d/common-password
 
 # Create a new user
 echo "Creating a new user..."
